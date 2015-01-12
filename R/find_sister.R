fb3180c9ff60efb57176878bf0ba71e4c9d65803
Francois Michonneau <francois.michonneau@gmail.com>
2015-01-12 10:54:58 -0700

    move `find_sister` and co to its own file

---
+++ R/find_sister.R
@@ -0,0 +1,58 @@
+find_sister <- function(tree = "data/raxml-epa-input/RAxML_labelledTree.epaTest") {
+
+    tr <- ape::read.tree(file = tree)
+    tr$edge.length <- NULL
+    tr <- as(tr, "phylo4")
+
+    qry <- grep("^QUERY", tipLabels(tr))
+
+    res <- vector("list", length(qry))
+    names(res) <- tipLabels(tr)[qry]
+
+    for (i in seq_along(qry)) {
+        cur_node <- qry[i]
+        while(TRUE) {
+            anc_node <- ancestor(tr, cur_node)
+            chi_node <- children(tr, anc_node)
+            check_nds <- descendants(tr, chi_node[chi_node != cur_node])
+            if (length(check_nds) > 1 &&
+                length(grep("^QUERY", names(check_nds))) == 0) {
+                break
+            } else {
+                cur_node <- anc_node
+            }
+        }
+        res[[i]] <- descendants(tr, anc_node)
+    }
+    res[!duplicated(res)]
+}
+
+rm_duplicate_seq <- function(sis_seq) {
+
+    ## only the QUERY sequences should be duplicated, the other ones
+    ## were checked during the creation of the alignment
+    res <- lapply(sis_seq, function(x) {
+        ufnb <- regmatches(names(x), gregexpr("UF_?[0-9]+", names(x)))
+        ufnb <- gsub("_", "", ufnb)
+        x[!duplicated(ufnb)]
+    })
+    res
+}
+
+is_arabia <- function(sis_seq) {
+
+    cukeDB <- load_cukeDB()
+    res <- lapply(sis_seq, function(x) {
+        latlong <- cukeDB[match(gsub("_[0-9]+amb$", "", names(x)),
+                                cukeDB$Labels), c("decimalLatitude", "decimalLongitude")]
+        ## Red Sea + Gulf of Aden polygon corners:
+        ## lat: 30 -- long: 30
+        ## lat: 3  -- long: 62
+        isara <- ifelse(latlong$decimalLatitude > 3 & latlong$decimalLatitude < 30 &
+                            latlong$decimalLongitude > 30 & latlong$decimalLongitude < 100,
+                        "arabia", "outside")
+        isara[grep("^QUERY", names(x))] <- "arabia"
+        isara
+    })
+
+}
--- R/make_epa_alg.R
+++ R/make_epa_alg.R
@@ -46,8 +46,8 @@ normalize_tree <- function(alg = "data/raxml-epa-input/epa-input.phy.reduced",
     tr
 }
 
-reduced_tree <- normalize_tree()
-ape::write.tree(reduced_tree, file="data/raxml-epa-input/reduced_tree.phy")
+#reduced_tree <- normalize_tree()
+#ape::write.tree(reduced_tree, file="data/raxml-epa-input/reduced_tree.phy")
 
 run_epa <- function(alg = "epa-input.phy",
                     tree = "RAxML_bestTree.cukeBarcodes",
@@ -55,76 +55,5 @@ run_epa <- function(alg = "epa-input.phy",
 
     system(paste("cd data/raxml-epa-input/; /home/francois/Software/RAxML-8.1.15/./raxmlHPC-PTHREADS-SSE3 -f v -s", alg, "-t", tree,
                  "-m", model, "-n", prefix, "-T7"))
-
-}
-
-
-find_sister <- function(tree = "data/raxml-epa-input/RAxML_labelledTree.epaTest") {
-
-    tr <- ape::read.tree(file = tree)
-    tr$edge.length <- NULL
-    tr <- as(tr, "phylo4")
-
-    qry <- grep("^QUERY", tipLabels(tr))
-
-    res <- vector("list", length(qry))
-    names(res) <- tipLabels(tr)[qry]
-
-    for (i in seq_along(qry)) {
-        cur_node <- qry[i]
-        while(TRUE) {
-            anc_node <- ancestor(tr, cur_node)
-            chi_node <- children(tr, anc_node)
-            check_nds <- descendants(tr, chi_node[chi_node != cur_node])
-            if (length(check_nds) > 1 &&
-                length(grep("^QUERY", names(check_nds))) == 0) {
-                break
-            } else {
-                cur_node <- anc_node
-            }
-        }
-        res[[i]] <- descendants(tr, anc_node)
-    }
-    res[!duplicated(res)]
-}
-
-make_find_sister <- function(overwrite=FALSE) {
-    fnm <- "data/find_sister.rds"
-    if (file.exists(fnm) && !overwrite) {
-        res <- readRDS(fnm)
-    } else {
-        res <- find_sister()
-        saveRDS(res, file=fnm)
-    }
-    res
-}
-
-rm_duplicate_seq <- function(sis_seq) {
-
-    ## only the QUERY sequences should be duplicated, the other ones
-    ## were checked during the creation of the alignment
-    res <- lapply(sis_seq, function(x) {
-        ufnb <- regmatches(names(x), gregexpr("UF_?[0-9]+", names(x)))
-        ufnb <- gsub("_", "", ufnb)
-        x[!duplicated(ufnb)]
-    })
-    res
-}
-
-is_arabia <- function(sis_seq=rm_duplicate_seq(make_find_sister())) {
-
-    cukeDB <- load_cukeDB()
-    res <- lapply(sis_seq, function(x) {
-        latlong <- cukeDB[match(gsub("_[0-9]+amb$", "", names(x)),
-                                cukeDB$Labels), c("decimalLatitude", "decimalLongitude")]
-        ## Red Sea + Gulf of Aden polygon:
-        ## lat: 30 -- long: 30
-        ## lat: 3  -- long: 62
-        isara <- ifelse(latlong$decimalLatitude > 3 & latlong$decimalLatitude < 30 &
-                            latlong$decimalLongitude > 30 & latlong$decimalLongitude < 100,
-                        "arabia", "outside")
-        isara[grep("^QUERY", names(x))] <- "arabia"
-        isara
-    })
 
 }
