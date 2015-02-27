8ce9277b88f05b833dee6f8af8985ee9a4c02d8b
Francois Michonneau <francois.michonneau@gmail.com>
2015-02-26 19:36:44 -0500

    refactor code
    * create function whose job is to return a list containing sequence and
      goegraphical origin of individuals
    * use results from this function to:
      - generate list of IM files
      - return list of nexus files to be used for BEAST analysis

--- R/find_sister.R
+++ R/find_sister.R
@@ -54,9 +54,6 @@ is_arabia <- function(sis_seq, cukeDB) {
     isara
 }
 
-make_im_file <- function(seqs, output_dir="data/im_files",
-                         alg_file, cukeDB, to_ignore_file, to_add_file) {
-
 read_alg_file <- function(alg_file) {
     ## Get complete alignment used to build tree
     alg <- ape::read.dna(file=alg_file, format="sequential", as.character=TRUE,
@@ -64,11 +61,45 @@ read_alg_file <- function(alg_file) {
     alg
 }
 
-    ## Create directory if non-existing
-    if (! file.exists(output_dir)) {
-        message("creating output_dir", output_dir)
-        dir.create(output_dir)
-    }
+get_pop_names <- function(pop) {
+    sort(na.omit(unique(pop)))
+}
+
+
+check_sequences <- function(seqs) {
+
+    to_keep <- sapply(seqs, function(seq) {
+
+        tmp_seqs <- seq[["sequences"]]
+        tmp_pop <- seq[["populations"]]
+        pop_names <- get_pop_names(tmp_pop)
+
+        ## At least 2 sequences for each species
+        if (length(tmp_seqs) < 2) {
+            message("not enough sequences for ", dimnames(tmp_seqs)[[1]][1])
+            return(FALSE)
+        }
+
+        ## At least 2 populations represented
+        if (length(pop_names) > 2) {
+            stop("there should only be 2 populations")
+        } else if (length(pop_names) == 1) {
+            message("not enough populations for ", dimnames(tmp_seqs)[[1]][1])
+            return(FALSE)
+        }
+
+        if (any(is.na(tmp_pop)))
+            stop("There are NAs in populations")
+
+        if (nrow(tmp_seqs) != length(tmp_pop))
+            stop("not the same number of sequences and populations")
+
+        return(TRUE)
+    })
+    to_keep
+}
+
+extract_sequences <- function(seqs, alg, cukeDB, to_ignore_file, to_add_file) {
 
     to_ignore <- scan(file=to_ignore_file, what="character")
     to_ignore <- paste0(to_ignore, "_?")
@@ -91,37 +122,87 @@ read_alg_file <- function(alg_file) {
         x
     })
 
-    pop <- lapply(pos_seq, function(x) is_arabia(x, cukeDB=cukeDB))
+    res_seq <- lapply(pos_seq, function(x) {
+        tmp_seq <- alg[x, ]
+        tmp_pop <- is_arabia(x, cukeDB=cukeDB)
+
+        ## Remove sequences for which the population can't be identified
+        if (any(is.na(tmp_pop))) {
+            tmp_seq <- tmp_seq[which(!is.na(tmp_pop)), ]
+            tmp_pop <- tmp_pop[which(!is.na(tmp_pop))]
+        }
+
+        list(sequences=tmp_seq,
+             populations=tmp_pop)
+    })
+
+    to_keep <- check_sequences(res_seq)
+    res_seq <- res_seq[to_keep]
+    res_seq
+}
+
+get_family <- function(seqs) {
+    lbls <- dimnames(seqs[["sequences"]])[[1]]
+    to_ignore <- grep("^RS_", lbls)
+    lbls <- lbls[-to_ignore]
+
+    fams <- strsplit(lbls, "_")
+    fams <- unique(sapply(fams, function(x) x[1]))
+    if (length(fams) != 1)
+        stop("More than 1 family for", lbls[1])
+    else
+        fams
+}
+
+make_nexus_files <- function(seqs, cukeDB, output_dir="data/nexus_files") {
+
+    ## Create directory if non-existing
+    if (! file.exists(output_dir)) {
+        message("creating output_dir", output_dir)
+        dir.create(output_dir)
+    }
+
+    fams <- sapply(seqs, get_family)
+
+    uniq_fam <- unique(fams)
+
+    out_files <- character(length(uniq_fam))
+
+    for (i in seq_along(uniq_fam)) {
+        idx_fam <- which(fams == uniq_fam[i])
+        tmp_seq <- lapply(seqs[idx_fam], function(x) x[["sequences"]])
+        tmp_pop <- lapply(seqs[idx_fam], function(x) x[["populations"]])
+        tmp_seq <- mapply(function(x, y, z) {
+            dimnames(x)[[1]] <- paste(dimnames(x)[[1]], y,
+                                      paste0("spp", z), sep="_")
+            x
+        }, tmp_seq, tmp_pop, seq_along(tmp_seq))
+        tmp_alg <- do.call("rbind", tmp_seq)
+        out_files[i] <- file.path(output_dir, paste0(uniq_fam[i], ".nex"))
+        ape::write.nexus.data(tmp_alg, file=out_files[i], interleaved=FALSE)
+    }
+    out_files
+}
+
+make_im_file <- function(seqs, output_dir="data/im_files", cukeDB) {
+
+    ## Create directory if non-existing
+    if (! file.exists(output_dir)) {
+        message("creating output_dir", output_dir)
+        dir.create(output_dir)
+    }
 
     list_files <- character(length(seqs))
     tree_labels <- vector("list", length(seqs))
 
-    ## filter out species species without enough individuals (can we do with only 1)
+    for (i in seq_along(seqs)) {
 
-    for (i in seq_along(pos_seq)) {
-
-        if (length(pos_seq[[i]]) < 2) {
-            message("not enough sequences for ", i)
-            next
-        }
-
-        pop_names <- na.omit(unique(pop[[i]]))
-        if (length(pop_names) > 2) {
-            stop("there should only be 2 populations")
-        } else if (length(pop_names) == 1) {
-            message("not enough populations for ", i)
-            next
-        }
-
-        pop_sizes <- as.numeric(table(pop[[i]])[pop_names])
-        loc_length <- dim(alg)[2]
-
-        ## order sequences according to their respective populations
-
-        sub_seq <- alg[pos_seq[[i]], ]
-        sub_seq <- sub_seq[c(which(pop[[i]] == pop_names[1]),    # to reorder the sequences
-                             which(pop[[i]] == pop_names[2])), ]
+        sub_seq <- seqs[[i]][["sequences"]]
         tree_labels[[i]] <- dimnames(sub_seq)[[1]]
+        sub_pop <- seqs[[i]][["populations"]]
+        pop_sizes <- as.numeric(table(sub_pop)[get_pop_names(sub_pop)])
+
+        loc_length <- dim(sub_seq)[2]
 
         ## extract species name for 1 line of file and file name
         spp_name <- strsplit(gsub("^QUERY___", "", dimnames(sub_seq)[[1]]), "_")
@@ -134,7 +215,7 @@ read_alg_file <- function(alg_file) {
 
         ## converts sequence names to something short
         rs_names <- grep("^RS_", dimnames(sub_seq)[[1]])
-        match_names <- match(dimnames(sub_seq)[[1]], cukeDB$Labels)
+        match_names <- match(dimnames(sub_seq)[[1]], cukeDB[["Labels"]])
         new_names <- cukeDB[match_names, "Sample"]
         new_names[rs_names] <- gsub("(.+)_+(UF_?[0-9]+)(_.+)?$", "\\2",
                                     dimnames(sub_seq)[[1]][rs_names])
@@ -146,12 +227,7 @@ read_alg_file <- function(alg_file) {
                  paste(dimnames(sub_seq)[[1]][is.na(new_names)], collapse=", "))
         }
 
-        if (length(new_names) == length(pop[[i]][!is.na(pop[[i]])])) {
-            new_names <- paste0(new_names, abbreviate(pop[[i]][!is.na(pop[[i]])],
-                                                      minlength=1))
-        } else {
-            stop("i = ", i, ". problem with NAs")
-        }
+        new_names <- paste0(new_names, abbreviate(sub_pop, minlength=1))
 
         ## clean up the sequence names
         new_names <- gsub("UF_?", "", new_names)
@@ -180,7 +256,7 @@ read_alg_file <- function(alg_file) {
         cat(spp_name, "\n", file=output)
 
         ## line 2: population names
-        cat(paste(pop_names, collapse=" "), "\n", file=output, append=TRUE)
+        cat(paste(get_pop_names(sub_pop), collapse=" "), "\n", file=output, append=TRUE)
 
         ## line 3: an integer representing the number of loci
         ## here only mtDNA, so 1
@@ -196,7 +272,7 @@ read_alg_file <- function(alg_file) {
             file=output, append=TRUE)
 
         ## line 5: sequence data
-             cat(sub_seq, sep="\n", file=output, append=TRUE)
+        cat(sub_seq, sep="\n", file=output, append=TRUE)
     }
     res <- list_files[nzchar(list_files)]
     attr(res, "tree_labels") <- tree_labels
@@ -237,6 +313,7 @@ check_tree_labels <- function(im_files, tree_file, dest) {
 run_convertIM <- function(infile="infile.list", wd="data/im_files") {
     system(paste0("cd ", wd, ";", "convertIM.pl ", infile))
 }
+
 make_obsSS <- function(batch_file="batch.masterIn.fromIM",
                        output="obsSS.txt", wd="data/im_files") {
     system(paste("cd", wd, ";",
