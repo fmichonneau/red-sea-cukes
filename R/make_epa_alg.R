2c960735d90fa999a950740e2d2d6098cae97c0e
Francois Michonneau <francois.michonneau@gmail.com>
2016-02-10 15:05:35 -0500

    add/redo EPA using newest sequences

--- R/make_epa_alg.R
+++ R/make_epa_alg.R
@@ -1,21 +1,25 @@
-##' function to create the alignemnt to be used with the Evolutionary
+##' Function to create the alignment to be used with the Evolutionary
 ##' Placement Algorithm in RAxML (places sequence in Tree). It needs a
 ##' tree and an alignment that contains the sequences used for the
 ##' tree + the sequences to be assigned.  The original alignment and
 ##' tree come from the barcoding chapter in dissertation. The file
 ##' with the sequences to be assigned were sent to me by Gustav in
-##' fasta format.  This function (1) takes both files, (2) adds prefix
-##' to sequences to assign to find them more easily a posteriori, (3)
-##' align the files
+##' fasta format (and I added on 2016-02-10 the latest sequences from
+##' the Red Sea/Oman).
+##'
+##'
+##' This function (1) takes both files, (2) adds prefix to sequences
+##' to assign to find them more easily a posteriori, (3) align the
+##' files
 ##' @param orig_alg the original alignment used to build the ML tree
-##' used
+##'     used
 ##' @param assign_alg the sequences to be assigned in the tree
 make_epa_alg <- function(orig_alg = "data/raxml-epa-input/cukeBarcodes-flagAmb.phy",
                          assign_alg = "data/raxml-epa-input/Cukes_Arabia.fas",
                          output = "data/raxml-epa-input/epa-input.phy") {
 
     orig_ape <- ape::read.dna(file=orig_alg, format="sequential")
-    assign_ape <- ape::read.dna(file=assign_alg, format="fasta")
+    assign_ape <- ape::read.dna(file=assign_alg, format="fasta", as.matrix = TRUE)
 
     dimnames(assign_ape)[[1]] <- paste0("RS_", dimnames(assign_ape)[[1]])
 
@@ -35,22 +39,23 @@ make_epa_alg <- function(orig_alg = "data/raxml-epa-input/cukeBarcodes-flagAmb.p
     invisible(file.exists(output))
 }
 
-## changed manually "immature" in tree and "immature in alg to _immature
-
-normalize_tree <- function(alg = "data/raxml-epa-input/epa-input.phy.reduced",
-                           tree = "data/raxml-epa-input/RAxML_bestTree.cukeBarcodes") {
+## This function is needed to remove one of the taxa (Stichopus aff
+## horrens from Galapagos that is not spelled correctly).
+normalize_tree <- function(alg = "data/raxml-epa-input/epa-input.phy",
+                           tree = "data/raxml-epa-input/RAxML_bestTree.cukeBarcodes",
+                           outtree = "data/raxml-epa-input/RAxML_bestTree.cukeBarcodes.normalized") {
     tr <- ape::read.tree(file=tree)
     alg <- ape::read.dna(file=alg, format="sequential")
     toDrop <- tr$tip.label[!tr$tip.label %in% dimnames(alg)[[1]]]
+    message("Dropping: ", paste(toDrop, collapse = ", "))
     tr <- ape::drop.tip(tr, toDrop)
-    tr
+    ape::write.tree(tr, file = outtree)
+    invisible(file.exists(outtree))
 }
 
-#reduced_tree <- normalize_tree()
-#ape::write.tree(reduced_tree, file="data/raxml-epa-input/reduced_tree.phy")
 
 run_epa <- function(alg = "epa-input.phy",
-                    tree = "RAxML_bestTree.cukeBarcodes",
+                    tree = "RAxML_bestTree.cukeBarcodes.normalized",
                     model="GTRCAT", prefix="epaTest") {
 
     system(paste("cd data/raxml-epa-input/; /home/francois/Software/RAxML-8.1.15/./raxmlHPC-PTHREADS-SSE3 -f v -s", alg, "-t", tree,
